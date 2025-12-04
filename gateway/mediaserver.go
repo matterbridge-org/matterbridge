6c41effb3841fc98a2d0fa0ace6867dbf3c263f8
Tadeusz Magura-Witkowski <tadeuszmw@gmail.com>
2025-12-04 21:10:49 +0100

    MediaServer: remove minio client, which appears to bring dependencies back in time, use proper S3 client

--- gateway/mediaserver.go
+++ gateway/mediaserver.go
@@ -13,11 +13,13 @@ import (
 	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go/logging"
+
 	"github.com/42wim/matterbridge/bridge/config"
 	"github.com/sirupsen/logrus"
-
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 type mediaServer interface {
@@ -42,10 +44,10 @@ type localMediaServer struct {
 	httpDownloadPrefix string
 }
 
-type minioMediaServer struct {
+type s3MediaServer struct {
 	commonMediaServer
 
-	minio          *minio.Client
+	s3client       *s3.Client
 	bucket         string
 	uploadPrefix   string
 	downloadPrefix string
@@ -55,7 +57,88 @@ type minioMediaServer struct {
 
 var _ mediaServer = (*httpPutMediaServer)(nil)
 var _ mediaServer = (*localMediaServer)(nil)
-var _ mediaServer = (*minioMediaServer)(nil)
+var _ mediaServer = (*s3MediaServer)(nil)
+
+func simpleS3Config(access_key, secret_access_key string, endpoint_url string) aws.Config {
+	return aws.Config{
+		Region:       "custom",
+		Credentials:  credentials.NewStaticCredentialsProvider(access_key, secret_access_key, ""),
+		Logger:       logging.Nop{},
+		BaseEndpoint: aws.String(endpoint_url),
+	}
+}
+
+func createS3MediaServer(bg *config.BridgeValues, parsed *url.URL, logger *logrus.Entry) (*s3MediaServer, error) {
+	optionsFromURL := parsed.Query()
+	secretAccessKey, _ := parsed.User.Password()
+	pathSplitted := strings.Split(strings.TrimLeft(parsed.Path, "/"), "/")
+
+	useSSL, err := strconv.ParseBool(optionsFromURL.Get("useSSL"))
+	if err != nil {
+		logger.Warn("error while parsing useSSL boolean, assuming false: ", err)
+		useSSL = false
+	}
+
+	pathStyle := true
+	if optionsFromURL.Has("pathStyle") {
+		pathStyle, err = strconv.ParseBool(optionsFromURL.Get("pathStyle"))
+		if err != nil {
+			logger.Warn("error while parsing pathStyle boolean, assuming false: ", err)
+			pathStyle = false
+		}
+	}
+
+	if len(pathSplitted) == 0 {
+		return nil, fmt.Errorf("no bucket specified")
+	}
+
+	bucketName := pathSplitted[0]
+	uploadPrefix := strings.Join(pathSplitted[1:], "/")
+
+	ctx := context.Background()
+
+	var s3BaseUrl string
+	if useSSL {
+		s3BaseUrl = "https://" + parsed.Host
+	} else {
+		s3BaseUrl = "http://" + parsed.Host
+	}
+
+	s3cfg := simpleS3Config(parsed.User.Username(), secretAccessKey, s3BaseUrl)
+	client := s3.NewFromConfig(s3cfg, func(o *s3.Options) {
+		o.UsePathStyle = pathStyle
+	})
+
+	logger.WithFields(logrus.Fields{
+		"bucket":       bucketName,
+		"uploadPrefix": uploadPrefix,
+	}).Debug("configured minio client")
+
+	// This will return an error if the bucket does not exist
+	headBucketResult, err := client.HeadBucket(ctx, &s3.HeadBucketInput{Bucket: aws.String(bucketName)})
+	if err != nil {
+		return nil, fmt.Errorf("failed checking if bucket exists: %w", err)
+	}
+
+	logger.WithFields(logrus.Fields{
+		"bucket":           bucketName,
+		"uploadPrefix":     uploadPrefix,
+		"headBucketResult": headBucketResult,
+	}).Debug("checked destination bucket")
+
+	return &s3MediaServer{
+		commonMediaServer: commonMediaServer{
+			logger: logger,
+		},
+
+		ctx:      ctx,
+		s3client: client,
+
+		bucket:         bucketName,
+		uploadPrefix:   uploadPrefix,
+		downloadPrefix: bg.General.MediaServerDownload,
+	}, nil
+}
 
 func createMediaServer(bg *config.BridgeValues, logger *logrus.Entry) (mediaServer, error) {
 	if bg.General.MediaServerUpload == "" && bg.General.MediaDownloadPath == "" {
@@ -79,59 +162,13 @@ func createMediaServer(bg *config.BridgeValues, logger *logrus.Entry) (mediaServ
 			}, nil
 		}
 
-		if parsed.Scheme == "minio" {
-			optionsFromURL := parsed.Query()
-			secretAccessKey, _ := parsed.User.Password()
-			pathSplitted := strings.Split(strings.TrimLeft(parsed.Path, "/"), "/")
-
-			useSSL, err := strconv.ParseBool(optionsFromURL.Get("useSSL"))
-			if err != nil {
-				logger.Warn("error while parsing useSSL boolean, assuming false: ", err)
-				useSSL = false
+		if parsed.Scheme == "s3" {
+			s3MediaServer, err := createS3MediaServer(bg, parsed, logger)
+			if err == nil {
+				return s3MediaServer, nil
 			}
 
-			if len(pathSplitted) == 0 {
-				return nil, fmt.Errorf("no bucket specified")
-			}
-
-			bucketName := pathSplitted[0]
-			uploadPrefix := strings.Join(pathSplitted[1:], "/")
-
-			ctx := context.Background()
-
-			minioClient, err := minio.New(parsed.Host, &minio.Options{
-				Creds:  credentials.NewStaticV4(parsed.User.Username(), secretAccessKey, ""),
-				Secure: useSSL,
-			})
-			if err != nil {
-				return nil, fmt.Errorf("failed to initialize minio client: %w", err)
-			}
-
-			logger.WithFields(logrus.Fields{
-				"bucket":       bucketName,
-				"uploadPrefix": uploadPrefix,
-			}).Debug("configured minio client")
-
-			exist, err := minioClient.BucketExists(ctx, bucketName)
-			if err != nil {
-				return nil, fmt.Errorf("failed checking if bucket exists: %w", err)
-			}
-			if !exist {
-				return nil, fmt.Errorf("specified bucket does not exists")
-			}
-
-			return &minioMediaServer{
-				commonMediaServer: commonMediaServer{
-					logger: logger,
-				},
-
-				ctx:   ctx,
-				minio: minioClient,
-
-				bucket:         bucketName,
-				uploadPrefix:   uploadPrefix,
-				downloadPrefix: bg.General.MediaServerDownload,
-			}, nil
+			return nil, fmt.Errorf("failed to configure s3 media server: %w", err)
 		}
 
 		return nil, fmt.Errorf("unknown schema (protocol) for mediaServerUpload: '%s'", parsed.Scheme)
@@ -198,18 +235,26 @@ func (h *localMediaServer) handleFilesUpload(fi *config.FileInfo) (string, error
 	return h.httpDownloadPrefix + "/" + sha1sum + "/" + fi.Name, nil
 }
 
-// handleFilesUpload which uploads media to minio compatible server (S3)
+// handleFilesUpload which uploads media to s3 compatible server.
 // Returns error on failure.
-func (h *minioMediaServer) handleFilesUpload(fi *config.FileInfo) (string, error) {
+func (h *s3MediaServer) handleFilesUpload(fi *config.FileInfo) (string, error) {
 	sha1sum := fmt.Sprintf("%x", sha1.Sum(*fi.Data))[:8]
-	url := h.uploadPrefix + "/" + sha1sum + "/" + fi.Name
+	key := h.uploadPrefix + "/" + sha1sum + "/" + fi.Name
 	objectSize := int64(len(*fi.Data)) // TODO: Using this, since we got this in memory anyway. Would be nicer to use fi.Size, but it is 0
 
-	info, err := h.minio.PutObject(h.ctx, h.bucket, url, bytes.NewReader(*fi.Data), objectSize, minio.PutObjectOptions{ContentType: "application/octet-stream"})
+	// We do not bother with multipart uploads for now, as files are expected to be small (less than 5GB).
+	// If needed, we can implement that later.
+	info, err := h.s3client.PutObject(h.ctx, &s3.PutObjectInput{
+		Bucket:        aws.String(h.bucket),
+		Key:           aws.String(key),
+		Body:          bytes.NewReader(*fi.Data),
+		ContentLength: aws.Int64(objectSize),
+		ContentType:   aws.String("application/octet-stream"),
+	})
 	if err != nil {
-		return "", fmt.Errorf("mediaserver putfile failed: %w", err)
+		return "", fmt.Errorf("mediaserver s3 putfile failed: %w", err)
 	}
 
-	h.logger.Debugf("successfully uploaded %v, etag: %v", url, info.ETag)
-	return h.downloadPrefix + url, nil
+	h.logger.Debugf("successfully uploaded %v, etag: %v", key, info.ETag)
+	return h.downloadPrefix + key, nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ require (
 	github.com/Benau/tgsconverter v0.0.0-20210809170556-99f4a4f6337f
 	github.com/Rhymen/go-whatsapp v0.1.2-0.20211102134409-31a2e740845c
 	github.com/SevereCloud/vksdk/v2 v2.17.0
+	github.com/aws/aws-sdk-go-v2 v1.40.1
+	github.com/aws/aws-sdk-go-v2/credentials v1.19.3
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.93.0
+	github.com/aws/smithy-go v1.24.0
 	github.com/bwmarrin/discordgo v0.28.1
 	github.com/d5/tengo/v2 v2.17.0
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc
@@ -27,7 +31,6 @@ require (
 	github.com/mattn/go-mastodon v0.0.10
 	github.com/mattn/godown v0.0.1
 	github.com/mdp/qrterminal v1.0.1
-	github.com/minio/minio-go/v7 v7.0.97
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/nelsonken/gomf v0.0.0-20190423072027-c65cc0469e94
 	github.com/olahol/melody v1.2.1
@@ -39,18 +42,18 @@ require (
 	github.com/sirupsen/logrus v1.9.3
 	github.com/slack-go/slack v0.14.0
 	github.com/spf13/viper v1.19.0
-	github.com/stretchr/testify v1.9.0
+	github.com/stretchr/testify v1.11.1
 	github.com/vincent-petithory/dataurl v1.0.0
 	github.com/writeas/go-strip-markdown v2.0.1+incompatible
 	github.com/xmppo/go-xmpp v0.2.18
 	github.com/yaegashi/msgraph.go v0.1.4
 	github.com/zfjagann/golang-ring v0.0.0-20220330170733-19bcea1b6289
-	go.mau.fi/whatsmeow v0.0.0-20240821142752-3d63c6fcc1a7
+	go.mau.fi/whatsmeow v0.0.0-20251116104239-3aca43070cd4
 	golang.org/x/image v0.19.0
 	golang.org/x/oauth2 v0.22.0
-	golang.org/x/text v0.30.0
+	golang.org/x/text v0.31.0
 	gomod.garykim.dev/nc-talk v0.3.0
-	google.golang.org/protobuf v1.34.2
+	google.golang.org/protobuf v1.36.10
 	layeh.com/gumble v0.0.0-20221205141517-d1df60a3cc14
 	modernc.org/sqlite v1.32.0
 )
@@ -61,13 +64,23 @@ require (
 	github.com/Jeffail/gabs v1.4.0 // indirect
 	github.com/apex/log v1.9.0 // indirect
 	github.com/av-elier/go-decimal-to-rational v0.0.0-20191127152832-89e6aad02ecf // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.9.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.15 // indirect
+	github.com/beeper/argo-go v1.1.2 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/coder/websocket v1.8.14 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/dyatlov/go-opengraph/opengraph v0.0.0-20220524092352-606d7b1e5f8a // indirect
+	github.com/elliotchance/orderedmap/v3 v3.1.0 // indirect
 	github.com/fatih/color v1.17.0 // indirect
 	github.com/francoispqt/gojay v1.2.13 // indirect
 	github.com/go-asn1-ber/asn1-ber v1.5.7 // indirect
-	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -81,20 +94,16 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
 	github.com/kettek/apng v0.0.0-20191108220231-414630eed80f // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.11 // indirect
-	github.com/klauspost/crc32 v1.3.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattermost/go-i18n v1.11.1-0.20211013152124-5c415071e404 // indirect
 	github.com/mattermost/ldap v0.0.0-20231116144001-0f480c025956 // indirect
 	github.com/mattermost/logr/v2 v2.0.21 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
-	github.com/minio/crc64nvme v1.1.0 // indirect
-	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/monaco-io/request v1.0.5 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
@@ -103,14 +112,15 @@ require (
 	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/petermattis/goid v0.0.0-20250904145737-900bdf8bb490 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240612014219-fbbf4953d986 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rickb777/date v1.12.4 // indirect
 	github.com/rickb777/plural v1.2.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/shazow/rateio v0.0.0-20200113175441-4461efc8bdc4 // indirect
@@ -121,22 +131,23 @@ require (
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	github.com/tinylib/msgp v1.3.0 // indirect
+	github.com/tinylib/msgp v1.2.0 // indirect
 	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.31 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/wiggin77/merror v1.0.5 // indirect
 	github.com/wiggin77/srslog v1.0.1 // indirect
-	go.mau.fi/libsignal v0.1.1 // indirect
-	go.mau.fi/util v0.6.0 // indirect
+	go.mau.fi/libsignal v0.2.1 // indirect
+	go.mau.fi/util v0.9.3 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.43.0 // indirect
-	golang.org/x/exp v0.0.0-20240707233637-46b078467d37 // indirect
-	golang.org/x/net v0.46.0 // indirect
-	golang.org/x/sys v0.37.0 // indirect
-	golang.org/x/term v0.36.0 // indirect
+	golang.org/x/crypto v0.44.0 // indirect
+	golang.org/x/exp v0.0.0-20251113190631-e25ba8c21ef6 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/term v0.37.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240722135656-d784300faade // indirect
 	google.golang.org/grpc v1.65.0 // indirect
