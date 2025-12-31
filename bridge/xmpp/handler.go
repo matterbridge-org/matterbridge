5a7ddfd03e0e4118cd0c7ebd22e76cf4c88054a2
selfhoster1312 <selfhoster1312@kl.netlib.re>
2025-12-31 11:46:48 +0100

    xmpp: Announce file sharer and caption before OOB URL

--- bridge/xmpp/handler.go
+++ bridge/xmpp/handler.go
@@ -32,3 +32,49 @@ func (b *Bxmpp) handleDownloadAvatar(avatar xmpp.AvatarData) {
 		b.Remote <- rmsg
 	}
 }
+
+// handleUploadFile handles native upload of files from other bridges/channels
+//
+// Implementation notes:
+//
+//   - some clients only display a preview when the body is exactly the URL, not only contains it.
+//     https://docs.modernxmpp.org/client/protocol/#communicating-the-url (Gajim/Conversations),
+//     so we need to produce a different message with the caption
+//   - the message body may or may not be different from an attachment's caption, and should
+//     therefore be sent separately:
+//     https://github.com/matterbridge-org/matterbridge/issues/50#issuecomment-3703478547
+//
+// This method does not return an error, because it will log errors as they happen,
+// and keep trying to send the other attachments if a previous one failed.
+func (b *Bxmpp) handleUploadFile(msg *config.Message) {
+	room := msg.Channel + "@" + b.GetString("Muc")
+
+	if msg.Text != "" {
+		// There's a message body. Maybe there's also an attachment caption, but maybe not.
+		// Let's print the body and the sender first, before iterating over attachments.
+		text := msg.Username + msg.Text
+
+		_, err := b.xc.Send(xmpp.Chat{
+			Type:   "groupchat",
+			Remote: room,
+			Text:   text,
+		})
+		if err != nil {
+			b.Log.WithError(err).Warnf("Skipping file announce due to failed body announce %s", text)
+			return
+		}
+	}
+
+	for _, file := range msg.Extra["file"] {
+		fileInfo := file.(config.FileInfo) //nolint: forcetypeassert
+		if fileInfo.URL != "" {
+			// The file already has a URL, either because the origin bridge provided it,
+			// or the file was reuploaded to matterbridge's mediaserver (if enabled).
+			// In this case, no need to reupload the file.
+			b.announceUploadedFile(msg.Channel+"@"+b.GetString("Muc"), msg.Username+fileInfo.Comment, fileInfo.Comment, fileInfo.URL)
+		} else {
+			// TODO
+			b.Log.Warn("OOB file upload unimplemented yet")
+		}
+	}
+}
